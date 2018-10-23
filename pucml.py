b250191e740767f44d4d892145c653428cb1cfb5
Yibo Jiang <yjiang@Yibos-MacBook-Pro-2.local>
2018-10-23 17:39:13 -0400

    basic model framework created

--- pucml.py
+++ pucml.py
@@ -1,35 +1,44 @@
 import numpy as np
-from misc import citeulike, split_data
+from misc import citeulike, split_data, define_scope
 import tensorflow as tf
-class PUCML_Base():
-    def __init__(self, prior=.7, lam=1):
-        self.prior = prior
-        self.lam = lam
+import argparse
 
-    def fit(self, x, y):
+class PUCML_Base():
+    def __init__(self,config,features=None):
+        # hyper-parameter
+        self.beta = config.beta   # tolerance margin
+        self.gamma = config.gamma # disconted learning rate
+
+        # dataset parameter
+        self.n_users = config.n_users
+        self.n_items = config.n_items
+
+
+        if features is not None:
+            self.features = tf.constant(features, dtype=tf.float32)
+        else:
+            # will be changed to random initialization later
+            self.emb_dim = 100
+            self.features = tf.Variable(tf.random_normal([self.n_items, self.embed_dim],
+                                            stddev=1 / (self.embed_dim ** 0.5), dtype=tf.float32))
+
+    def input_dataset_pipeline(self):
+        pass
+    def prior_estimation(self):
+        # placeholder value 0.5
+        return 0.5
+
+    def sample_base_item_pairs(self):
         pass
 
-    def predict(self, x):
-        check_is_fitted(self, 'coef_')
-        x = check_array(x)
-        return np.sign(.1 + np.sign(self._basis(x).dot(self.coef_)))
+    def model(self):
+        # compute nearest neigbors
 
-    def score(self, x, y):
-        x_s, x_u = x[y == 1, :], x[y == 0, :]
-        f = self.predict
-        p_p = self.prior
-        p_n = 1 - self.prior
+        # compute score functions
 
-        # SU risk estimator with zero-one loss
-        r_s = (np.sign(-f(x_s)) - np.sign(f(x_s))) * p_p / (p_p - p_n)
-        r_u = (-p_n * (1 - np.sign(f(x_u))) + p_p * (1 - np.sign(-f(x_u)))) / (p_p - p_n)
-        return r_s.mean() + r_u.mean()
+        # tf.cond for different optimization
 
-    def _basis(self, x):
-        # linear basis
-        return np.hstack((x, np.ones((len(x), 1))))
-
-def main_algo():
+def main_algo(config):
     # get user-item matrix
     user_item_matrix, features = citeulike(tag_occurence_thres=5)
     n_users, n_items = user_item_matrix.shape
@@ -37,8 +46,32 @@ def main_algo():
     dense_features = features.toarray() + 1E-10
     # get train/valid/test user-item matrices
     train, valid, test = split_data(user_item_matrix)
+
+    # add a few stuff to config
+    config.n_users = n_users
+    config.n_items = n_items
+
+    pucml_learner = PUCML_Base(config,features=dense_features)
     # O,U = batch
     # while True:
     #     pass
 
-main_algo()
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--beta',
+        action   = 'store',
+        required = True,
+        type     = float,
+        default  = 0.5,
+        help     = 'tolerance margin')
+
+    parser.add_argument('--gamma',
+        action   = 'store',
+        required = True,
+        type     = float,
+        default  = 0.5,
+        help     = 'disconted learning rate')
+
+    config = parser.parse_args()
+    main_algo(config)
