472a65654f233260daa8be24e4abd12f75c9b2b1
Yibo Jiang <yjiang@Yibos-MacBook-Pro-2.local>
2018-11-08 18:33:17 -0500

    adding train recall

--- pucml.py
+++ pucml.py
@@ -210,7 +210,7 @@ class PUCML_Base():
         """ define loss and optimization """
         # define two differnt losses and their optimizer
         # total_loss = self.prior * R_p_plus + (P_u_minus - self.prior * R_p_minus)+ tf.nn.l2_loss(self.alpha)# + self.feature_loss#
-        total_loss =  R_p_plus + (P_u_minus - R_p_minus) + self.feature_loss
+        total_loss =  R_p_plus + (P_u_minus - R_p_minus) # + self.feature_loss
         negative_loss = P_u_minus - self.prior * R_p_minus
 
         full_opt = tf.train.AdamOptimizer(learning_rate=self.lr).minimize(total_loss)
