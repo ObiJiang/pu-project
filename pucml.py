d13d91199f0bc0a1ff3ff1edc2fe4dabb05e64a4
Yibo Jiang <yjiang@Yibos-MacBook-Pro-2.local>
2018-11-12 00:54:52 -0500

    directly optimzing pnn_dist

--- pucml.py
+++ pucml.py
@@ -213,7 +213,7 @@ class PUCML_Base():
         total_loss =  R_p_plus + (P_u_minus - R_p_minus) # + self.feature_loss
         negative_loss = P_u_minus - self.prior * R_p_minus
 
-        full_opt = tf.train.AdamOptimizer(learning_rate=self.lr).minimize(pnn_dist_sum)
+        full_opt = tf.train.AdamOptimizer(learning_rate=self.lr).minimize(-1*pnn_dist_sum)
         neg_opt = tf.train.AdamOptimizer(learning_rate=self.lr*self.gamma).minimize(-1*negative_loss)
 
         # tf.cond for different optimization
