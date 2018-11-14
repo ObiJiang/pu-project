41e27fa841e8421c23ebda1a57e2fa11313d486e
Yibo Jiang <yjiang@Yibos-MacBook-Pro-2.local>
2018-11-14 15:00:22 -0500

    try simpler loss function for diagonal_nok

--- pucml_diagonal_nok.py
+++ pucml_diagonal_nok.py
@@ -173,7 +173,7 @@ class PUCML_Base():
         total_loss =  R_p_plus + (P_u_minus - R_p_minus) # + self.feature_loss
         negative_loss = P_u_minus - self.prior * R_p_minus
 
-        full_opt = tf.train.AdamOptimizer(learning_rate=self.lr).minimize(total_loss)
+        full_opt = tf.train.AdamOptimizer(learning_rate=self.lr).minimize(p_scores+u_scores)
         neg_opt = tf.train.AdamOptimizer(learning_rate=self.lr*self.gamma).minimize(-1*negative_loss)
 
         # tf.cond for different optimization
