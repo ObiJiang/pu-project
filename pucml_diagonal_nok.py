3064dfa49013043130bb270a26f9a5e7a86b8158
Yibo Jiang <yjiang@Yibos-MacBook-Pro-2.local>
2018-11-14 01:33:24 -0500

    update confidence score

--- pucml_diagonal_nok.py
+++ pucml_diagonal_nok.py
@@ -29,8 +29,8 @@ class PUCML_Base():
 
         # prior
         self.prior = self.prior_estimation()
-        self.prior_list_numpy = np.load('./prior_list.npy').astype(np.float32)
-        self.prior_list = tf.constant(self.prior_list_numpy)
+        #self.prior_list_numpy = np.load('./prior_list.npy').astype(np.float32)
+        #self.prior_list = tf.constant(self.prior_list_numpy)
 
         # dataset parameter
         self.n_users = config.n_users
@@ -130,63 +130,48 @@ class PUCML_Base():
         alpha_in_batch = tf.gather(self.alpha,p_u[:,0])
         metrics_in_batch = tf.linalg.diag(alpha_in_batch)
 
-        """ compute distances """
+        """ compute sum of positve distances """
         fea_in_batch = tf.gather(self.features,p_u[:,1:])
-        fea_diff_in_batch = tf.expand_dims(fea_in_batch,2) - self.features
-        dist_in_batch_part_1 = tf.einsum('bijm,bmn->bijn', fea_diff_in_batch, metrics_in_batch)
-        dist_in_batch = tf.negative(tf.einsum('bijn,bijn->bij', fea_diff_in_batch, dist_in_batch_part_1))
-
-        """ compute nearest neighbors """
-        lower_bound = tf.reduce_min(dist_in_batch)
         user_postive_ind_map_in_batch = tf.gather(self.user_postive_ind_map,p_u[:,0])
+        postive_dist = tf.gather(self.features,tf.nn.relu(user_postive_ind_map_in_batch))
+        fea_diff_in_batch_pos = tf.expand_dims(fea_in_batch,2) - tf.expand_dims(postive_dist,1)
 
-        # TO DO: max_n_p_elem can be optimized for each batch
-        user_index_in_batch = tf.tile(tf.expand_dims(tf.range(self.batch_size),axis=1),[1,self.max_n_p_elem])
-        user_item_postive_pair_ind_in_batch = tf.concat([tf.expand_dims(user_index_in_batch,axis=2),
-                                                         tf.expand_dims(user_postive_ind_map_in_batch,axis=2)],axis=2)
-        user_item_postive_pair_ind_in_batch_unroll = tf.reshape(user_item_postive_pair_ind_in_batch,[-1,2])
-        # eliminate all the -1 at the end
-        nonzero_indices = tf.where((user_item_postive_pair_ind_in_batch_unroll[:,0]+1)*user_item_postive_pair_ind_in_batch_unroll[:,1]>=0)
-        nonzero_values = tf.gather_nd(user_item_postive_pair_ind_in_batch_unroll, nonzero_indices)
+        dist_in_batch_part_1 = tf.einsum('bijm,bmn->bijn', fea_diff_in_batch_pos, metrics_in_batch)
+        dist_in_batch = tf.negative(tf.einsum('bijn,bijn->bij', fea_diff_in_batch_pos, dist_in_batch_part_1))
 
-        # create a big matrix where user-positve-item pairs have values of the lower bound
-        indices = nonzero_values
-        updates = tf.cast(tf.sign(nonzero_values[:,0]+1),tf.float32)*lower_bound
-        shape = tf.constant([self.batch_size, self.n_items])
-        scatter = tf.expand_dims(tf.scatter_nd(indices, updates, shape),axis=1)
+        user_item_bool = tf.cast(tf.expand_dims(tf.sign(user_postive_ind_map_in_batch + 1),axis=1),tf.float32)
+        pnn_dist_sum = tf.reduce_sum(dist_in_batch*user_item_bool,axis=1)
 
-        # compute postive nn
-        pnn_dist,_ = tf.nn.top_k(dist_in_batch - scatter,k=self.k+1) # +1 because the output will include itself (0 distance)
-        pnn_dist_filter = tf.nn.relu(pnn_dist) # non-postive items will not be above 0
-        pnn_dist_filter = tf.concat([pnn_dist_filter[:,0:1,1:],pnn_dist_filter[:,1:,:self.k]],axis=1)
+        """ compute sum of negative distances """
+        fea_in_batch = tf.gather(self.features,p_u[:,1:])
+        user_postive_ind_map_in_batch = tf.gather(self.user_postive_ind_map,p_u[:,0])
+        postive_dist = tf.gather(self.features,tf.nn.relu(user_postive_ind_map_in_batch))
+        fea_diff_in_batch_pos = tf.expand_dims(fea_in_batch,2) - tf.expand_dims(postive_dist,1)
 
-        nonnegative_indices = tf.tile(tf.expand_dims(tf.sign(user_postive_ind_map_in_batch + 1)[:,:self.k+1],axis=1),
-                                     [1,1+self.n_unlabeled,1])
-        nonnegative_indices = tf.concat([nonnegative_indices[:,0:1,1:],nonnegative_indices[:,1:,:self.k]],axis=1)
-        pnn_dist_sum = tf.reduce_sum(pnn_dist_filter,axis=2) +\
-                       tf.reduce_sum(tf.cast(nonnegative_indices,tf.float32)*lower_bound,axis=2)
+        dist_in_batch_part_1 = tf.einsum('bijm,bmn->bijn', fea_diff_in_batch_pos, metrics_in_batch)
+        dist_in_batch = tf.negative(tf.einsum('bijn,bijn->bij', fea_diff_in_batch_pos, dist_in_batch_part_1))
 
-        # compute unlabeled nn
-        unn_dist,_ = tf.nn.top_k(dist_in_batch + scatter,k=self.k+1)
-        unn_dist = tf.concat([unn_dist[:,0:1,:self.k],unn_dist[:,1:,1:]],axis=1)
-        unn_dist_sum = tf.reduce_sum(unn_dist,axis=2)
+        user_item_bool = tf.cast(tf.expand_dims(tf.sign(user_postive_ind_map_in_batch + 1),axis=1),tf.float32)
+        unn_dist_sum = tf.reduce_sum(dist_in_batch*user_item_bool,axis=1)
+
+        with tf.Session() as sess:
+            sess.run(tf.global_variables_initializer())
+            train_handle = sess.run(train_iterator.string_handle())
+            sess.run(train_iterator.initializer)
+            print(sess.run(pnn_dist_sum, feed_dict = {handle: train_handle}).shape)
 
         """ compute score functions """
-        confidence_scores = tf.exp(pnn_dist_sum)/(tf.exp(pnn_dist_sum)+tf.exp(unn_dist_sum))
-        # confidence_scores = pnn_dist_sum/(pnn_dist_sum+unn_dist_sum) # linear version
+        # confidence_scores = tf.exp(pnn_dist_sum)/(tf.exp(pnn_dist_sum)+tf.exp(unn_dist_sum))
+        confidence_scores = pnn_dist_sum/(pnn_dist_sum+unn_dist_sum) # linear version
         #confidence_scores = tf.log(pnn_dist_sum)-tf.log(pnn_dist_sum+unn_dist_sum) # log version
 
         p_scores = confidence_scores[:,0]
         u_scores = confidence_scores[:,1:]
 
-        prior_in_batch =  tf.gather(self.prior_list,p_u[:,0])
-        # R_p_plus = tf.reduce_mean(1/(1 + tf.exp(p_scores)))*prior_in_batch
-        # R_p_minus = tf.reduce_mean(1/(1 + tf.exp(-1*p_scores)))*prior_in_batch
-        # P_u_minus = tf.reduce_mean(1/(1 + tf.exp(-1*u_scores)))
+        #prior_in_batch =  tf.gather(self.prior_list,p_u[:,0])
 
-
-        R_p_plus = tf.reduce_mean(-1*tf.log(1+p_scores))*prior_in_batch
-        R_p_minus = tf.reduce_mean(-1*tf.log(2-p_scores))*prior_in_batch
+        R_p_plus = tf.reduce_mean(-1*tf.log(1+p_scores))*0.2
+        R_p_minus = tf.reduce_mean(-1*tf.log(2-p_scores))*0.2
         P_u_minus = tf.reduce_mean(-1*tf.log(2-u_scores))
 
         # R_p_plus = tf.reduce_mean(-1*pnn_dist_sum)
@@ -239,7 +224,7 @@ class PUCML_Base():
         val_model = self.val_model
         valid_users = np.random.choice(list(set(self.valid.nonzero()[0])), size=1000, replace=False)
         train_users = np.random.choice(list(set(self.train.nonzero()[0])), size=1000, replace=False)
-        # validation_recall = RecallEvaluator_knn(val_model, self.train, self.valid, self.test_batch_size)
+
         validation_recall = RecallEvaluator(val_model, self.train, self.valid)
         train_recall = RecallEvaluator(val_model, self.valid, self.train)
 
@@ -248,8 +233,6 @@ class PUCML_Base():
         configPro.gpu_options.allow_growth = True
 
         with tf.Session(config=configPro) as sess:
-            #sess = tf_debug.TensorBoardDebugWrapperSession(sess,'128.59.176.64:6064')
-            #sess = tf_debug.LocalCLIDebugWrapperSession(sess)
             sess.run(tf.global_variables_initializer())
 
             train_handle = sess.run(model.train_iterator.string_handle())
@@ -277,19 +260,9 @@ class PUCML_Base():
                 for loop_idx in tqdm(range(int(self.total_num_user_item/self.batch_size)), desc="Optimizing..."):
                     _, loss = sess.run((model.selctive_opt, model.total_loss),
                                        feed_dict = {model.handle: train_handle})
-                    # _, _, loss = sess.run((model.update_features,model.update_alpha, model.total_loss),
-                    #                    feed_dict = {model.handle: train_handle})
-                    # # if loop_idx % 2 == 0:
-                    #     _, loss = sess.run((model.update_features, model.total_loss),
-                    #                        feed_dict = {model.handle: train_handle})
-                    # else:
-                    #     _, loss = sess.run((model.update_alpha, model.total_loss),
-                    #                        feed_dict = {model.handle: train_handle})
-                    # print(sess.run([self.alpha,model.pnn_dist_sum,model.unn_dist_sum,model.p_scores],feed_dict = {model.handle: train_handle}))
                     losses.append(loss)
                     if loop_idx%self.evaluation_loop_num == 0:
-                        # print(sess.run(model.alpha_in_batch,feed_dict = {model.handle: train_handle}))
-                        # print(sess.run([model.pnn_dist_sum,model.unn_dist_sum],feed_dict = {model.handle: train_handle}))
+
                         valid_recalls = []
                         """ Evaluation recall@k """
                         for user_chunk in toolz.partition_all(10, valid_users):
