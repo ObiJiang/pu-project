03377ef7e0351fec84a4fdb0ea1091c7b3451f8a
Yibo Jiang <jyb940413@gmail.com>
2018-11-29 12:06:49 -0500

    debugging prior

--- pucml_diagonal_weighed_ach.py
+++ pucml_diagonal_weighed_ach.py
@@ -173,8 +173,8 @@ class PUCML_Base():
         neg_opt = tf.train.AdamOptimizer(learning_rate=self.lr*self.gamma).minimize(-1*negative_loss)
 
         # tf.cond for different optimization
-        selctive_opt = tf.cond(negative_loss > self.beta, lambda: full_opt, lambda: neg_opt)
-        #selctive_opt = full_opt
+        #selctive_opt = tf.cond(negative_loss > self.beta, lambda: full_opt, lambda: neg_opt)
+        selctive_opt = full_opt
 
         return AttrDict(locals())  # The magic line.
 
